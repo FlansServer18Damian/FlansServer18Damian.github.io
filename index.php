4814809c5acef37e0fafd886615eed9679a197e4
damianfurrer <damianfurrer@bluewin.ch>
2016-05-31 16:28:41 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,1 @@
-<meta http-equiv="refresh" content="0; URL=http://<?php echo file_get_contents('ip.txt');?>/">
+<meta http-equiv="refresh" content="0; URL=http://<?php echo google.ch;//echo file_get_contents('ip.txt');?>/">
